0f62ebfd998912292f54f4f0596563269581d796
gilbert-ku <gilbert45ku@gmail.com>
2023-09-06 01:11:13 +0300

    add student table

--- lib/modules.py
+++ lib/modules.py
@@ -1,5 +1,5 @@
 # print("hello wold")
-
+from sqlalchemy import Column, Integer, String, Sequence
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 
@@ -8,3 +8,11 @@ Base = declarative_base()
 DATABASE_URI = 'sqlite:///school.db'
 engine = create_engine(DATABASE_URI, echo=True)
 
+class Students(Base):
+    __tablename__ = "students"
+    student_id = Column(Integer, Sequence("student_id_seq"), primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    date_of_birth = (Integer)
+    address = (String)
+    Contact_infornation  = (Integer)
