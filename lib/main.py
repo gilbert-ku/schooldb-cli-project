09a6288c7d5ba0c8a9e0d79ab4c57785cc52f3e0
gilbert-ku <gilbert45ku@gmail.com>
2023-09-06 10:43:44 +0300

    create env to add data in the database

--- lib/main.py
+++ lib/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+#define connection
+
+DATABASE_URI = 'sqlite:///school.db'
+engine = create_engine (DATABASE_URI, echo=True)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+session.close()
