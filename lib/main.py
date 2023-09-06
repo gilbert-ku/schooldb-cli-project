fd546d796114a46a2fcc3b7f84e16e7fc8793394
gilbert-ku <gilbert45ku@gmail.com>
2023-09-06 23:14:42 +0300

    install and imort click library

--- lib/main.py
+++ lib/main.py
@@ -1,17 +1,50 @@
-#!/usr/bin/env python3
-
+# install and import click library
+import click
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 
+from modules import Student, GradeLookup, grade_lookup_data
 
-#define connection
-
+# Define the database connection
 DATABASE_URI = 'sqlite:///school.db'
-engine = create_engine (DATABASE_URI, echo=True)
+engine = create_engine(DATABASE_URI, echo=True)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
+@click.command()
+@click.option('--first-name', prompt='Enter first name', required=True)
+@click.option('--last-name', prompt='Enter last name', required=True)
+@click.option('--date-of-birth', prompt='Enter date of birth (YYYY-MM-DD)', required=True)
+@click.option('--address', prompt='Enter address', required=True)
+@click.option('--contact-information', prompt='Enter contact information', required=True)
 
 
-session.close()
+def add_student(first_name, last_name, date_of_birth, address, contact_information):
+    try:
+        # Convert date_of_birth to a Python date object
+        date_of_birth = datetime.strptime(date_of_birth, '%Y-%m-%d').date()
+
+        # Create a new student object
+        new_student = Student(
+            first_name=first_name,
+            last_name=last_name,
+            date_of_birth=date_of_birth,
+            address=address,
+            contact_information=contact_information,
+        )
+
+        # Add the student to the database
+        session.add(new_student)
+
+        # Commit the changes
+        session.commit()
+        print("Student added successfully!")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        session.rollback()
+
+if __name__ == '__main__':
+    add_student()
--- lib/modules.py
+++ lib/modules.py
@@ -4,34 +4,31 @@ from sqlalchemy.orm import declarative_base, relationship
 
 # Create a base class for declarative models
 Base = declarative_base()
-DATABASE_URI = 'sqlite:///school.db'
-engine = create_engine(DATABASE_URI, echo=True)
+DATABASE_URI = 'sqlite:///school.db'  # Update with your preferred database URI
+engine = create_engine(DATABASE_URI, echo=True)  # Set echo to False in production
 
 class Student(Base):
     __tablename__ = "students"
+    # Add a unique constraint for contact information
     __table_args__ = (
-        UniqueConstraint("contact_information",
-            name="unique_contact_information"),
-        CheckConstraint("grade BETWEEN A AND F",
-            name="grade_between_A_and_F")
+        UniqueConstraint("contact_information", name="unique_contact_information"),
     )
     student_id = Column(Integer, Sequence("student_id_seq"), primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
     date_of_birth = Column(Date)  
     address = Column(String)
-    contact_information = Column(Integer) 
+    contact_information = Column(String)  # Changed to String for contact info
 
-    # Define the relationship with CourseEnrollment
+    # Define the relationship with CourseEnrollment and Grade
     enrollments = relationship("CourseEnrollment", back_populates="student")
     grades = relationship("Grade", back_populates="student")
 
 class Teacher(Base):
-
     __tablename__ = "teachers"
+    # Add a unique constraint for contact information
     __table_args__ = (
-        UniqueConstraint("contact_information",
-            name="unique_contact_information")
+        UniqueConstraint("contact_information", name="unique_contact_information"),
     )
     teacher_id = Column(Integer, Sequence("teacher_id_seq"), primary_key=True)
     first_name = Column(String)
@@ -50,13 +47,14 @@ class Course(Base):
     teacher_id = Column(Integer, ForeignKey("teachers.teacher_id"))
 
     # Define the relationship with CourseEnrollment and Grade
+    teacher = relationship("Teacher", back_populates="courses")
     enrollments = relationship("CourseEnrollment", back_populates="course")
     grades = relationship("Grade", back_populates="course")
 
 class CourseEnrollment(Base):
     __tablename__ = "course_enrollments"
     enrollment_id = Column(Integer, Sequence("enrollment_id_seq"), primary_key=True)
-    enrollment_date = Column(Date)  # You might want to use a different data type
+    enrollment_date = Column(Date)
     student_id = Column(Integer, ForeignKey("students.student_id"))
     course_id = Column(Integer, ForeignKey("courses.course_id"))
 
@@ -67,14 +65,17 @@ class CourseEnrollment(Base):
 class Grade(Base):
     __tablename__ = "grades"
     grade_id = Column(Integer, Sequence("grade_id_seq"), primary_key=True)
-    date_of_grade = Column(Date)  # You might want to use a different data type
+    date_of_grade = Column(Date)
     student_id = Column(Integer, ForeignKey("students.student_id"))
     course_id = Column(Integer, ForeignKey("courses.course_id"))
-    grade = Column(Integer)
-
+    
+    # Store the grade as an integer (0-100)
+    grade_value = Column(Integer)
+    
     # Define the relationships with Student and Course
     student = relationship("Student", back_populates="grades")
     course = relationship("Course", back_populates="grades")
+
 
 # Create the tables in the database
 Base.metadata.create_all(engine)
